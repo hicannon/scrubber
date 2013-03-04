2f0a30620dde3d152c244a23f112824acc712bc1
hicannon <hi_cannon@yahoo.com>
2013-03-03 22:43:01 -0500

    remove personal build files

--- build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/detian/Dropbox/CS4758/rosws/scrubber/build/devel/setup.sh"
--- build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/home/detian/Dropbox/CS4758/rosws/scrubber/build/devel/setup.sh"
-emulate zsh # back to zsh mode
