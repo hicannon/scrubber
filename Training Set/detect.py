d525184adcd672ac333caa1757cc969bd61a523c
hicannon <hi_cannon@yahoo.com>
2013-03-31 17:10:13 -0400

    fix basic stain dection

--- Training Set/detect.py
+++ Training Set/detect.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageMath, ImageOps, ImageFilter
+import sys
 
 def loadImage(fname):
     return Image.open(fname)
@@ -34,22 +35,28 @@ def detectStains(fname, windowSize=50):
     """Basic difference method: subtract out each window with average window, then
     filter by average difference"""
     res = []
-    for x in range(sx / windowSize):
-        res.append([])
-        for y in range(sy / windowSize):
+    for y in range(sx / windowSize):
+        #res.append([])
+        for x in range(sy / windowSize):
+            xp = x*windowSize
+            yp = y*windowSize
             window = im.crop((xp,yp,xp+windowSize, yp+windowSize))
+            #window.show()
+            #raw_input("")
             temp = ImageMath.eval("float(a)-float(b)",a=window, b=avg)
             temp2 = temp.load()
             diff = 0
             for c in range(windowSize):
                 for r in range(windowSize):
                     diff+=pow(temp2[r,c],2.0)
-            res[x].append(diff)
+            #res[x].append(diff)
+            res.append(diff)
     #find average difference
     accum = 0
-    for r in res:
+    accum = sum(res)
+    """for r in res:
         for i in r:
-            accum+=i
+            accum+=i"""
     accum/=count
     
     #subtract out average
@@ -57,14 +64,32 @@ def detectStains(fname, windowSize=50):
     dmin = 1000000000.0
     for x in range(sx/windowSize):
         for y in range(sy/windowSize):
-            res[x][y]-=accum
-            res[x][y] = abs(res[x][y])
-#    print res
-    for row in res:
-        dmax = max(max(res[x]),dmax)
-        dmin = min(min(res[x]),dmin)
-    vis = Image.new("L", (len(res), len(res[0])), None)
-    vis.putdata(res, 255/dmax, -dmin)
+            res[x+y*(sx/windowSize)]-=accum
+            res[x+y*(sx/windowSize)] = abs(res[x+y*(sx/windowSize)])
+    #print res
+    """for row in res:
+        #print dmax,dmin
+        dmax = max(max(row),dmax)
+        dmin = min(min(row),dmin)"""
+    dmax = max(res)
+    dmin = min(res)
+    vis = Image.new("L", (sx/windowSize, sy/windowSize), None)
+    temp = dmax-dmin
+    if temp==0:
+        temprange =0
+    else:
+        temprange = 255/temp
+    #print res
+    print "dmin",dmin
+    print "dmax",dmax
+    print "max",temprange
+    print len(res)
+    vis.putdata(res, temprange, -dmin*(255/(-dmin+dmax)))
+    temp = vis.load()
+    """for x in range(sx/windowSize):
+        for y in range(sx/windowSize):
+            print sys.stdout.write(str(temp[x,y])+" ")
+        print ' '"""
     vis.show()        
 
-detectStains("2013-03-05 17.01.45.jpg", 50)
+detectStains("2013-03-12 16.44.49.jpg", 50)
